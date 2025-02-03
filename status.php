1617affd5dd04be0de5aed2d62fda19f52c582ef
Stuart Ledingham <68251154+OnlineDynamic@users.noreply.github.com>
2025-02-03 23:34:27 +0000

    Update status.php to remove external bootstrap reference

--- status.php
+++ status.php
@@ -34,10 +34,7 @@ foreach ($leagues as $league) {
 <!DOCTYPE html>
 <html>
 <head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-    crossorigin="anonymous">
+
   <style>
     #bodyWrapper {
       background-color: #20222e;
