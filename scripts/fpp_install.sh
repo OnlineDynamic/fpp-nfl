08b5796870804d0455ac9756b3d262dd8f56afb7
Stuart Ledingham <68251154+OnlineDynamic@users.noreply.github.com>
2025-02-03 23:40:02 +0000

    Add espn domain to trusted domains for CSP security

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Include common scripts functions and variables
+. ${FPPDIR}/scripts/common
+
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
 /opt/fpp/scripts/update_plugin ${target_PWD##*/}
-echo ; echo “Please reboot fppd.” ; echo
-. /opt/fpp/scripts/common
-setSetting rebootFlag 1
 popd
+
+# Add required Apache CSP (Content-Security-Policy allowed domains
+${FPPDIR}/scripts/ManageApacheContentPolicy.sh add img-src https://a.espncdn.com
+
+# Need to force reboot for CSP change to take affect
+setSetting rebootFlag 1
+
+echo ; echo “Please reboot fppd.” ; echo
